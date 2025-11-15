556c0f51128825d5ad99184958338b36ea95faec
asif-kamal <asif.kamal1489@gmail.com>
2025-11-14 19:32:26 -0600

    Turtle package manipulation - draw a dashed line

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 
 import turtle as t
 
+
 timmy_the_turtle = t.Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.backward(200)
-timmy_the_turtle.right(90)
-timmy_the_turtle.left(180)
-timmy_the_turtle.setheading(0)
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("red")
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.backward(200)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.left(180)
+# timmy_the_turtle.setheading(0)
 
 
 ######## Challenge 1 - Draw a Square ############
@@ -17,8 +18,15 @@ timmy_the_turtle.setheading(0)
 timmy_the_turtle.pendown()
 timmy_the_turtle.pencolor("green")
 
-for _ in range(4):
-    timmy_the_turtle.forward(200)
-    timmy_the_turtle.right(90)
+# for _ in range(4):
+#     timmy_the_turtle.forward(200)
+#     timmy_the_turtle.right(90)
+
+for _ in range(25):
+    timmy_the_turtle.pu()
+    timmy_the_turtle.forward(5)
+    timmy_the_turtle.pd()
+    timmy_the_turtle.forward(5)
+
 
 t.exitonclick()

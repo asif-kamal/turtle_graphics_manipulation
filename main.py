da9278c161cfcc42cebcd537a4dcda57698a435b
asif-kamal <asif.kamal1489@gmail.com>
2025-11-16 16:48:15 -0600

    Turtle package manipulation - draw in random directions in random colors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #####Turtle Intro######
-
+from turtle import Screen
 import turtle as t
+import random
 
 
 timmy_the_turtle = t.Turtle()
@@ -28,44 +29,64 @@ timmy_the_turtle.pencolor("green")
 #     timmy_the_turtle.pd()
 #     timmy_the_turtle.forward(5)
 
+
+
 #triangle
-for _ in range(3):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(360/3)
+# for _ in range(3):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(360/3)
+#
+# #square
+# for _ in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(360/4)
+#
+# #pentagon
+# for _ in range(5):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(360/5)
+#
+# #6-sided
+# for _ in range(6):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(360/6)
+#
+# #7-sided
+# for _ in range(7):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(360/7)
+#
+# #octagon
+# for _ in range(8):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(360/8)
+#
+# #nonagon
+# for _ in range(9):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(360/9)
+#
+# #decagon
+# for _ in range(10):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.left(360/10)
 
-#square
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(360/4)
+s = Screen()
 
-#pentagon
-for _ in range(5):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(360/5)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
 
-#6-sided
-for _ in range(6):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(360/6)
+timmy_the_turtle.speed(4)
+timmy_the_turtle.pensize(10)
+timmy_the_turtle.pendown()
+t.colormode(255)
 
-#7-sided
-for _ in range(7):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(360/7)
+for _ in range(100):
+    timmy_the_turtle.forward(50)
+    timmy_the_turtle.left(random.choice([90, 180, 270]))
+    timmy_the_turtle.color(random_color())
 
-#octagon
-for _ in range(8):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(360/8)
-
-#nonagon
-for _ in range(9):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(360/9)
-
-#decagon
-for _ in range(10):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(360/10)
-
-t.exitonclick()
+s.exitonclick()
